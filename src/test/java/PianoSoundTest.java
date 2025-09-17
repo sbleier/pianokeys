d4dab95fe18b4c6a009b16425bf610013af368fc
Tkachuk, Adel <tkachukadel@gmail.com>
2025-09-17 18:03:41 -0400

    created a branch

---
+++ src/test/java/PianoSoundTest.java
@@ -0,0 +1,66 @@
+import javax.sound.midi.MidiUnavailableException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PianoSoundTest
+{
+    public static void main(String[] args) throws MidiUnavailableException
+    {
+        PianoSound piano = new PianoSound();
+
+        try
+        {
+            // Play each note for half a second
+            for (int i = 0; i < 6; i += 2)
+            {
+                System.out.println("Playing note " + i);
+                piano.playNote(i);
+                Thread.sleep(1000); // Wait 500ms
+                piano.stopNote(i);
+                Thread.sleep(500); // Small gap between notes
+            }
+
+            Thread.sleep(1000);
+
+            piano.playNote(0);
+            piano.playNote(2);
+            piano.playNote(4);
+
+            Thread.sleep(3000);
+
+            piano.stopNote(0);
+            piano.stopNote(2);
+            piano.stopNote(4);
+
+            for (int i = 1; i < 7; i += 2)
+            {
+                System.out.println("Playing note " + i);
+                piano.playNote(i);
+                Thread.sleep(1000); // Wait 500ms
+                piano.stopNote(i);
+                Thread.sleep(500); // Small gap between notes
+            }
+
+            Thread.sleep(1000);
+
+            piano.playNote(1);
+            piano.playNote(3);
+            piano.playNote(5);
+
+            Thread.sleep(3000);
+
+            piano.stopNote(1);
+            piano.stopNote(3);
+            piano.stopNote(5);
+        } catch (
+                InterruptedException e)
+
+        {
+            e.printStackTrace();
+        } finally
+
+        {
+            piano.cleanup();
+        }
+    }
+}
