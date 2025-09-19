da9e50bdb515590e6ccb5018ec52a3bbe88b63b9
Tkachuk, Adel <tkachukadel@gmail.com>
2025-09-18 23:12:51 -0400

    changes the notes constants names

--- src/main/java/pianokeys/PianoSound.java
+++ src/main/java/pianokeys/PianoSound.java
@@ -5,20 +5,20 @@ import javax.sound.midi.*;
 public class PianoSound
 {
     // MIDI note constants
-    private final int C4 = 60;
-    private final int D4 = 62;
-    private final int E4 = 64;
-    private final int F4 = 65;
-    private final int G4 = 67;
-    private final int A4 = 69;
-    private final int B4 = 71;
-    private final int C5 = 72;
+    private final int NOTE_C4 = 60;
+    private final int NOTE_D4 = 62;
+    private final int NOTE_E4 = 64;
+    private final int NOTE_F4 = 65;
+    private final int NOTE_G4 = 67;
+    private final int NOTE_A4 = 69;
+    private final int NOTE_B4 = 71;
+    private final int NOTE_C5 = 72;
 
     private Synthesizer synthesizer;
     private MidiChannel channel;
 
     // Use the constants in your array
-    private int[] notes = {C4, D4, E4, F4, G4, A4, B4, C5};
+    private int[] notes = {NOTE_A4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5};
 
     public PianoSound() throws MidiUnavailableException
     {
