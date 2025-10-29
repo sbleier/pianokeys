4057044be3feda361469e6e5ccfaed1404a8166e
SARA BLEIER <sbleier2@student.touro.edu>
2025-10-29 17:45:58 -0400

    added play method to composition

--- src/main/java/pianokeys/Composition.java
+++ src/main/java/pianokeys/Composition.java
@@ -24,6 +24,27 @@ public class Composition
         noteList.addAll(Arrays.stream(notes).toList());
     }
 
+    public void play(PianoSound sound) {
+
+        //loop through noteList to play all notes
+        for (Note note: noteList) {
+            //duration of note in milliseconds
+            double duration = (note.endTime() - note.startTime()) * 1000;
+
+            //note starts to play
+            sound.playNote(note.key());
+
+            //note plays for duration
+            try {
+                Thread.sleep((long) duration);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            //stop note after sleep of duration
+            sound.stopNote(note.key());
+        }
+    }
 }
 
 
