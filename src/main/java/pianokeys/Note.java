862845a0e6a7420956b9f5f50ce28630e957790c
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2025-10-30 11:54:44 -0400

    Added SimpleCompositionFactory

--- src/main/java/pianokeys/Note.java
+++ src/main/java/pianokeys/Note.java
@@ -1,5 +1,23 @@
 package pianokeys;
 
-public record Note(int key, double startTime, double endTime)
-{
+/**
+ * @param key
+ * @param startTime in seconds
+ * @param endTime   in seconds
+ */
+public record Note(int key, double startTime, double endTime) {
+
+    /**
+     * All Notes should start and end in increments of 1/8 of a second.
+     */
+    public static final double TIME_STEP = 1 / 8.0;
+
+    /**
+     * @param time
+     * @return time rounded to the nearest 8th of a second.
+     */
+    public static double roundToNearestEight(double time) {
+        return Math.round(time * 8.0) / 8.0;
+    }
+
 }
