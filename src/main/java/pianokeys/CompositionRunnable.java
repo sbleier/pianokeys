b79da9314cc0b0eaf8fe2cc6cb7b5dabe634bc42
SARA BLEIER <sbleier2@student.touro.edu>
2025-11-02 14:06:39 -0500

    edited test

--- src/main/java/pianokeys/CompositionRunnable.java
+++ src/main/java/pianokeys/CompositionRunnable.java
@@ -2,7 +2,7 @@ package pianokeys;
 
 public class CompositionRunnable implements Runnable {
 
-    public static final double STEP = 1 / 8.0;
+    public double STEP = 1 / 8.0;
 
     private final PianoSound sound;
     private Composition composition;
@@ -10,6 +10,12 @@ public class CompositionRunnable implements Runnable {
     public CompositionRunnable(PianoSound sound, Composition composition) {
         this.sound = sound;
         this.composition = composition;
+    }
+
+    public CompositionRunnable(PianoSound sound, Composition composition, double step) {
+        this.sound = sound;
+        this.composition = composition;
+        STEP = step;
     }
 
     @Override
@@ -32,7 +38,7 @@ public class CompositionRunnable implements Runnable {
             try {
                 Thread.sleep((long) (STEP * 1000));
             } catch (InterruptedException e) {
-                System.err.println(e.getMessage());
+               throw new RuntimeException(e);
 
             }
 
--- src/test/java/pianokeys/CompositionRunnableTest.java
+++ src/test/java/pianokeys/CompositionRunnableTest.java
@@ -1,14 +1,14 @@
 package pianokeys;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
 import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Synthesizer;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class CompositionRunnableTest {
 
@@ -22,10 +22,12 @@ class CompositionRunnableTest {
         Composition comp = new Composition();
         comp.addNote(new Note(PianoSound.C4, 0.0, 0.25));
         comp.addNote(new Note(PianoSound.D4, 0.25, 0.5));
-        CompositionRunnable runnable = new CompositionRunnable(sound, comp);
+        CompositionRunnable runnable = new CompositionRunnable(sound, comp, 0.0);
 
-        //when
+
+        // when
         runnable.run();
+
 
         //then
         verify(channel).noteOn(PianoSound.C4, 127);
