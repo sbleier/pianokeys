4fee85093aad03fc8ff023db821f6ba35833d13d
leoraspinner <leoraspinner@gmail.com>
2025-11-05 19:01:51 -0500

    added controller

---
+++ src/main/java/pianokeys/PianoController.java
@@ -0,0 +1,40 @@
+package pianokeys;
+
+public class PianoController
+{
+    private CompositionView compositionView;
+    private PianoSound sound;
+    private Composition composition;
+
+    public PianoController(CompositionView compositionView, PianoSound sound, Composition composition) {
+        this.compositionView = compositionView;
+        this.sound = sound;
+        this.composition = composition;
+    }
+
+    public void playNote(int note)
+    {
+        if(sound != null){
+            sound.playNote(note);
+        }
+
+    }
+
+    public void stopNote(int note)
+    {
+        if(sound != null){
+            sound.stopNote(note);
+        }
+
+    }
+
+    public void playComposition()
+    {
+
+    }
+
+    public void pauseComposition()
+    {
+
+    }
+}
